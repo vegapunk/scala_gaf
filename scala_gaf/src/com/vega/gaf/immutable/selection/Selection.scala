0e47aa489005e28906b303e101691e1760f8fc2f
Amyl <amyldv@googlemail.com>
2013-02-16 17:10:03 +0000

    Added Selection trait

---
+++ scala_gaf/src/com/vega/gaf/immutable/selection/Selection.scala
@@ -0,0 +1,15 @@
+package com.vega.gaf.immutable.selection
+import com.vega.gaf.immutable.environment.Population
+
+/**
+ * Should be used to select which Chromosomes will go into the next round of iterations
+ */
+trait Selection[T] {
+
+	/**
+	 * Implementations should return a new population of the given size from the given
+	 * population
+	 */
+	def select(env:Population[T], size:Int) : Population[T]
+	
+}
