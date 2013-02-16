1714c2b5de286c2a0b99fe7538b4fa913dbeedba
Amyl <amyldv@googlemail.com>
2013-02-16 17:11:17 +0000

    Added scaladoc to Mutation trait

--- scala_gaf/src/com/vega/gaf/immutable/mutation/Mutation.scala
+++ scala_gaf/src/com/vega/gaf/immutable/mutation/Mutation.scala
@@ -2,8 +2,14 @@ package com.vega.gaf.immutable.mutation
 
 import com.vega.gaf.immutable.gene.Gene
 
+/**
+ * Interface for Mutation
+ */
 trait Mutation[T] {
 
+	/**
+	 * Implementations should return a mutated version of the given Gene
+	 */
 	def mutate(gene:Gene[T]): Gene[T]
-	
+
 }
