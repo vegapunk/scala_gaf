022369c74891b6600999bf123cca31edcb211b96
Amyl <amyldv@googlemail.com>
2013-02-16 16:52:07 +0000

    Added CrossSelector trait for choosing candidates for crossover

---
+++ scala_gaf/src/com/vega/gaf/immutable/crosschooser/CrossoverSelector.scala
@@ -0,0 +1,12 @@
+package com.vega.gaf.immutable.crosschooser
+import com.vega.gaf.immutable.chromosome.Chromosome
+import com.vega.gaf.immutable.environment.Population
+
+/**
+ * Should be used to select from a population a pair of Chromosomes to be crossed over
+ */
+trait CrossoverSelector[T] {
+
+	def selectForCrossover(pop: Population[T]): List[Pair[Chromosome[T], Chromosome[T]]]
+	
+}
---
+++ scala_gaf/src/com/vega/gaf/immutable/crosschooser/impl/RandomCrossSelector.scala
@@ -0,0 +1,23 @@
+package com.vega
+import com.vega.gaf.immutable.crosschooser.CrossoverSelector
+import com.vega.gaf.immutable.environment.Population
+import com.vega.gaf.immutable.chromosome.Chromosome
+import scala.util.Random
+
+/**
+ * Will select 2 Chromosomes at random from a population.
+ * The 2 chromosomes selected can potentially be identical
+ */
+trait RandomCrossSelector[T] extends CrossoverSelector[T]{
+
+	def selectForCrossover(pop: Population[T]): List[Pair[Chromosome[T], Chromosome[T]]] ={
+		def generatePair() : Pair[Chromosome[T], Chromosome[T]] = {
+			val chromA = pop.population(new Random().nextInt(pop.population.size))
+			val chromB = pop.population(new Random().nextInt(pop.population.size))
+			new Pair(chromA, chromB)
+		}
+		
+		List(generatePair())
+	}
+	
+}
