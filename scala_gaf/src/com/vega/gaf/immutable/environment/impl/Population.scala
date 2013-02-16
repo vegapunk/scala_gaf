f76b670c8463f634eaa3b542d567023d7e2de9bf
Amyl <amyldv@googlemail.com>
2013-02-16 15:10:45 +0000

    Changed class name of Environment to Population

--- scala_gaf/src/com/vega/gaf/immutable/environment/impl/Population.scala
+++ scala_gaf/src/com/vega/gaf/immutable/environment/impl/Population.scala
@@ -4,26 +4,26 @@ import com.vega.gaf.immutable.chromosome.Chromosome
 /**
  * Class encapsulates a population of Chromosomes of type T
  */
-protected class Environment[T](pop:List[Chromosome[T]]) {
+protected class Population[T](pop:List[Chromosome[T]]) {
 
 	val population = pop	
 }
 
-object Environment{
+object Population{
 	/**
 	 * Creates a new Environment from the given list of chromosomes
 	 */
-	def apply[T](pop:List[Chromosome[T]] ) : Environment[T] = new Environment(pop)
+	def apply[T](pop:List[Chromosome[T]] ) : Population[T] = new Population(pop)
 
 	/**
 	 * Creates a new Environment from the given environment and a list of Chromosomes, the Environments population
 	 * will contain all the members of the old population with the members of the new list
 	 */
-	def apply[T](env:Environment[T], newPop:List[Chromosome[T]]) = new Environment(newPop ::: env.population)
+	def apply[T](env:Population[T], newPop:List[Chromosome[T]]) = new Population(newPop ::: env.population)
 	
 	/**
 	 * Creates a new Environment from the given environment and a single Chromosome, the new Environment's population 
 	 * will contain all the members of the old Environment's population and the given Chromosome
 	 */
-	def apply[T](env:Environment[T], chrom:Chromosome[T]) = new Environment(chrom :: env.population)
+	def apply[T](env:Population[T], chrom:Chromosome[T]) = new Population(chrom :: env.population)
 }
