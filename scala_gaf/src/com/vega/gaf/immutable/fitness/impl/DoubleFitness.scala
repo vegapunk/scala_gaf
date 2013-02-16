6776b9fd03b7507ba8e2c6c5810c8f59527cccd4
Amyl <amyldv@googlemail.com>
2013-02-16 10:59:08 +0000

    package structure change

--- scala_gaf/src/com/vega/gaf/immutable/fitness/impl/DoubleFitness.scala
+++
@@ -1,9 +0,0 @@
-package com.vega.gaf.immutable.fitness.impl
-import com.vega.gaf.immutable.fitness.Fitness
-
-/**
- * Fitness that is measured as a double
- */
-trait DoubleFitness[T] extends Fitness[Double, T]{
-
-}
